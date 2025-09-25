05fe3926a44560160786e72d88c1d138af742e68
Vlad Loran <vladloran17@gmail.com>
2025-09-25 01:32:26 +0100

    Fixed few asked mistakes

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,18 +1,32 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
     private static final int DEFAULT_CAPACITY = 10;
-    private int storageSize = 0;
-    private K[] keyArray = (K[]) new Object[DEFAULT_CAPACITY];
-    private V[] valueArray = (V[]) new Object[DEFAULT_CAPACITY];
+    private int storageSize;
+    private final K[] keyArray;
+    private final V[] valueArray;
+
+    public StorageImpl() {
+        storageSize = 0;
+        keyArray = (K[]) new Object[DEFAULT_CAPACITY];
+        valueArray = (V[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    private int getIndexOfKey(K key) {
+        for (int i = 0; i < storageSize; i++) {
+            if (keyArray[i] == null ? key == null : keyArray[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     @Override
     public void put(K key, V value) {
         for (int i = 0; i < storageSize; i++) {
-            if (Objects.equals(keyArray[i], key)) {
+            if (keyArray[i] == null ? key == null : keyArray[i].equals(key)) {
                 valueArray[i] = value;
                 return;
             }
@@ -26,7 +40,7 @@ public class StorageImpl<K, V> implements Storage<K, V> {
     @Override
     public V get(K key) {
         for (int i = 0; i < storageSize; i++) {
-            if (Objects.equals(keyArray[i], key)) {
+            if (keyArray[i] == null ? key == null : keyArray[i].equals(key)) {
                 return valueArray[i];
             }
         }
