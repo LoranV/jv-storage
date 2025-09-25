deb4699cb039011ad7e447fdde677bf8a5d09067
Vlad Loran <vladloran17@gmail.com>
2025-09-25 01:01:17 +0100

    Finished task

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,19 +1,40 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
+import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private int storageSize = 0;
+    private K[] keyArray = (K[]) new Object[DEFAULT_CAPACITY];
+    private V[] valueArray = (V[]) new Object[DEFAULT_CAPACITY];
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < storageSize; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                valueArray[i] = value;
+                return;
+            }
+        }
+        if (storageSize < DEFAULT_CAPACITY) {
+            keyArray[storageSize] = key;
+            valueArray[storageSize++] = value;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < storageSize; i++) {
+            if (Objects.equals(keyArray[i], key)) {
+                return valueArray[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return storageSize;
     }
 }
